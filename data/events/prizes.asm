2bfd6fbd5bf10cba2607344b5721408951cc3735
Mato <codeknight4242@yahoo.ca>
2025-11-19 04:41:20 -0500

    Game Corner prizes updated, including fossils and post-game TMs

--- data/events/prizes.asm
+++ data/events/prizes.asm
@@ -2,6 +2,8 @@ PrizeDifferentMenuPtrs:
 	dw PrizeMenuMon1Entries, PrizeMenuMon1Cost
 	dw PrizeMenuMon2Entries, PrizeMenuMon2Cost
 	dw PrizeMenuTMsEntries,  PrizeMenuTMsCost
+	dw PrizeMenuPostGameTMsEntries,  PrizeMenuPostGameTMsCost
+	dw PrizeMenuFossilGruntEntries,  PrizeMenuFossilGruntCost
 
 NoThanksText:
 	db "NO THANKS@"
@@ -9,65 +11,25 @@ NoThanksText:
 PrizeMenuMon1Entries:
 	db ABRA
 	db CLEFAIRY
-IF DEF(_RED)
-	db NIDORINA
-ENDC
-IF DEF(_BLUE)
-	db NIDORINO
-ENDC
-IF DEF(_GREEN)
-	db NIDORINO
-ENDC
+	db DRATINI
 	db "@"
 
 PrizeMenuMon1Cost:
-IF DEF(_RED)
 	bcd2 180
 	bcd2 500
-ENDC
-IF DEF(_BLUE)
-	bcd2 120
-	bcd2 750
-ENDC
-IF DEF(_GREEN)
-	bcd2 120
-	bcd2 750
-ENDC
 	bcd2 1200
 	db "@"
 
 PrizeMenuMon2Entries:
-IF DEF(_RED)
-	db DRATINI
 	db SCYTHER
-ENDC
-IF DEF(_BLUE)
-	db PINSIR
-	db DRATINI
-ENDC
-IF DEF(_GREEN)
-	db PINSIR
-	db DRATINI
-ENDC
+	db EEVEE
 	db PORYGON
 	db "@"
 
 PrizeMenuMon2Cost:
-IF DEF(_RED)
-	bcd2 2800
-	bcd2 5500
-	bcd2 9999
-ENDC
-IF DEF(_BLUE)
-	bcd2 2500
-	bcd2 4600
-	bcd2 6500
-ENDC
-IF DEF(_GREEN)
-	bcd2 2500
-	bcd2 4600
-	bcd2 6500
-ENDC
+	bcd2 3200
+	bcd2 4400
+	bcd2 7777
 	db "@"
 
 PrizeMenuTMsEntries:
@@ -80,4 +42,28 @@ PrizeMenuTMsCost:
 	bcd2 3300
 	bcd2 5500
 	bcd2 7700
+	db "@"
+
+PrizeMenuPostGameTMsEntries:
+	db TM_DOUBLE_EDGE
+	db TM_SELFDESTRUCT
+	db TM_EXPLOSION
+	db "@"
+
+PrizeMenuPostGameTMsCost:
+	bcd2 3000
+	bcd2 6000
+	bcd2 8000
+	db "@"
+
+PrizeMenuFossilGruntEntries:
+	db HELIX_FOSSIL
+	db DOME_FOSSIL
+	db OLD_AMBER
+	db "@"
+
+PrizeMenuFossilGruntCost:
+	bcd2 5000
+	bcd2 5000
+	bcd2 9000
 	db "@"
