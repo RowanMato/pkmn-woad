e57284723e0122ceff6c5f34755ad0b89d9b785a
Mato <codeknight4242@yahoo.ca>
2025-11-23 02:54:30 -0500

    Removed some unused text

--- data/text/unused_names.asm
+++ data/text/unused_names.asm
@@ -9,14 +9,14 @@ UnusedBadgeNames::
 	db "ひのたまバッヂ@" ; FIREBALL BADGE
 	db "ゴールドバッヂ@" ; GOLD BADGE
 
-UnusedRankingNames:
-	db "たまご@" ; EGG
-	db "ひよこ@" ; CHICK
-	db "ブロンズ@" ; BRONZE
-	db "シルバー@" ; SILVER
-	db "ゴールド@" ; GOLD
-	db "プチキャプテン@" ; PETIT CAPTAIN
-	db "キャプテン@" ; CAPTAIN
-	db "プチマスター@" ; PETIT MASTER
-	db "マスター@" ; MASTER
-	db "エクセレント" ; EXCELLENT
+;UnusedRankingNames:
+;	db "たまご@" ; EGG
+;	db "ひよこ@" ; CHICK
+;	db "ブロンズ@" ; BRONZE
+;	db "シルバー@" ; SILVER
+;	db "ゴールド@" ; GOLD
+;	db "プチキャプテン@" ; PETIT CAPTAIN
+;	db "キャプテン@" ; CAPTAIN
+;	db "プチマスター@" ; PETIT MASTER
+;	db "マスター@" ; MASTER
+;	db "エクセレント" ; EXCELLENT
