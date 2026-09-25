9e722d72910d7034a403b3e47a3a100623045e9f
Mato <rowanmato@gmail.com>
2026-09-25 05:09:03 -0400

    Further attempts to fix the 1st Trainer bug, if I want proper fixing + Not important thanks to the prior workaround, but it would be nice to fix it properly in the future I guess. Doesn't affect the player either way.

--- main.asm
+++ main.asm
@@ -199,10 +199,8 @@ SECTION "Battle Engine 7", ROMX
 INCLUDE "data/moves/moves.asm"
 INCLUDE "data/pokemon/base_stats.asm"
 INCLUDE "data/pokemon/cries.asm"
-INCLUDE "engine/battle/unused_stats_functions.asm"
+INCLUDE "engine/battle/trainer_ai.asm"
 INCLUDE "engine/battle/scroll_draw_trainer_pic.asm"
-INCLUDE "engine/battle/draw_hud_pokeball_gfx.asm"
-INCLUDE "gfx/trade.asm"
 INCLUDE "engine/pokemon/evos_moves.asm"
 INCLUDE "engine/battle/move_effects/heal.asm"
 INCLUDE "engine/battle/move_effects/transform.asm"
@@ -211,7 +209,8 @@ INCLUDE "engine/battle/move_effects/reflect_light_screen.asm"
 
 SECTION "Battle Engine 12", ROMX
 
-INCLUDE "engine/battle/trainer_ai.asm"
+INCLUDE "gfx/trade.asm"
+INCLUDE "engine/battle/draw_hud_pokeball_gfx.asm"
 
 
 SECTION "Battle Core", ROMX
